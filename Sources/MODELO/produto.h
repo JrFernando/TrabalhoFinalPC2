828a27610768469aa025e909b84ccb277122dca0
baleeiro17 <lucasdominato2@gmail.com>
2017-06-29 10:45:10 -0300

    Add files via upload

---
+++ Sources/MODELO/produto.h
@@ -0,0 +1,24 @@
+/**
+ * Este módulo define as  funções de manipulações 
+ * da struct Produto.
+ *
+ * @author Fernando Júnior Gomes da Silva <fernandojunior20110@gmail.com>
+  @author Lucas Baleeiro Dominato <lucasdominato2@gmail.com>
+ 
+ * @date 27/06/2017
+*/
+#include "fabricante.h"
+ 
+typedef struct {
+	int id;
+	char *nome;
+ 	int quantidade;
+ 	float preco_compra;
+ 	float preco_venda;
+ 	Fabricante fabricante;
+}Produto;
+
+
+//Prototipos das funções produto:
+
+Produto lendo_produto(void);
