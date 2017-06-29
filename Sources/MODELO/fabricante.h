647a3205d9d9ff27a86f12ac987bfc9eb19170a2
baleeiro17 <lucasdominato2@gmail.com>
2017-06-29 10:30:27 -0300

    Struct fabricante e suas funções cadastro

---
+++ Sources/MODELO/fabricante.h
@@ -0,0 +1,18 @@
+/**
+ * Este módulo define a struct Fabricante e é responsável pelas funções de manipulações 
+ * da struct.
+ *
+ * @author Fernando Júnior Gomes da Silva <fernandojunior20110@gmail.com>
+  @author Lucas Baleeiro Dominato <lucasdominato2@gmail.com>
+ 
+ * @date 27/06/2017
+*/
+ 
+typedef struct {
+	int id;
+	char *nome;
+}Fabricante;
+
+
+//Prototípos das funções:
+Fabricante lendo_fabricante();
