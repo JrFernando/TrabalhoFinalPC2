027ba41ea9da36e718dffe45f8ddd7ac6a78d7ef
baleeiro17 <lucasdominato2@gmail.com>
2017-06-29 10:42:55 -0300

    Struct produto e suas funcoes cadastro

---
+++ Sources/MODELO/produto.h
@@ -0,0 +1,23 @@
+/**
+ * Este módulo define a struct Produto e é responsável pelas funções de manipulações 
+ * da struct.
+ *
+ * @author Fernando Júnior Gomes da Silva <fernandojunior20110@gmail.com>
+ * @date 27/06/2017
+*/
+ 
+#include "fabricante.h"
+ 
+typedef struct {
+	int id;
+	char *nome;
+ 	int quantidade;
+ 	float preco_compra;
+ 	float preco_venda;
+ 	Fabricante fabricante;
+}Produto;
+
+
+//Prototipos das funções produto:
+
+Produto lendo_produto(void);
