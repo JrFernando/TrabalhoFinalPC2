5402e6d4244f0008eafce6eba3215bd0ce288d97
baleeiro17 <lucasdominato2@gmail.com>
2017-07-01 12:49:11 -0300

    funções do cupom fiscal(para o caixa).

---
+++ Sources/MENU/cupom_fiscal/horario_cupom.h
@@ -0,0 +1,13 @@
+/**
+ * Este módulo define as funções que passaram o horario e a data do sistema.
+ * @author Fernando Júnior Gomes da Silva <fernandojunior20110@gmail.com>
+  @author Lucas Baleeiro Dominato <lucasdominato2@gmail.com>
+ 
+ * @date 27/06/2017
+*/
+#include<time.h>
+
+//prototipos das funcoes:
+
+char* gerando_horario_cupom();
+char* gerando_data_cupom();
