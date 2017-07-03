ebe06a34b37acf36f271c0574bdc532bbf66b0f0
JrFernando <fernandojunior20110@gmail.com>
2017-07-03 17:05:54 -0300

    removi arquivos de compilação

--- build/Debug/GNU-Linux/Sources/BD/bd.o.d
+++
@@ -1,6 +0,0 @@
-build/Debug/GNU-Linux/Sources/BD/bd.o: Sources/BD/bd.c Sources/BD/bd.h \
- Sources/BD/../UTIL/my_bool.h
-
-Sources/BD/bd.h:
-
-Sources/BD/../UTIL/my_bool.h:
--- build/Debug/GNU-Linux/Sources/BD/fabricante_bd.o.d
+++
@@ -1,2 +0,0 @@
-build/Debug/GNU-Linux/Sources/BD/fabricante_bd.o: \
- Sources/BD/fabricante_bd.c
