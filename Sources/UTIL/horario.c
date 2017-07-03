8c5ea973c69ea038d7018d5ae1d629a93f12c84d
JrFernando <fernandojunior20110@gmail.com>
2017-07-03 14:46:44 -0300

    Correção no harario.c e horario.h

--- Sources/UTIL/horario.c
+++ Sources/UTIL/horario.c
@@ -2,7 +2,7 @@
  * Este módulo define as funções que passaram o horario e a data do sistema.
  * @author Fernando Júnior Gomes da Silva <fernandojunior20110@gmail.com>
  * @author Lucas Baleeiro Dominato <lucasdominato2@gmail.com>
- 
+ *
  * @date 27/06/2017
 */
 #include<stdio.h>
@@ -16,6 +16,8 @@ tm* gerando_horario(void){
 	t=time(NULL);
 	local=localtime(&t);
 	
+	local->tm_mon += 1;
+	local->tm_year += 1900;
 	
 	return local;
 }
