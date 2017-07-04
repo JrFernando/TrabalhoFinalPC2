5c769fd5168b4cfc18d7b3f8a9aea71f509af90e
baleeiro17 <lucasdominato2@gmail.com>
2017-07-03 21:34:14 -0300

    funcoes planilhas-excel.

---
+++ Sources/UTIL/planilhas.h
@@ -0,0 +1,10 @@
+#include "acoes_funcionario.h"
+
+
+//protótipos de funções:
+void produtos_mais_vendidos(Produto *,int,char *);
+void produtos_mais_lucrativos(Produto *,int,char *);
+void produtos_e_estoque(Produto *,int,char *);
+void compras_realizadas(Compra *,int,char *);
+void receita_dos_caixas(Turno *,int,char *);
+void funcionario_que_mais_venderam(Funcionario *,int,char *);
