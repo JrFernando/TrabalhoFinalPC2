7fac6f6e022f87e18001a8131f23d3ba1b1439f8
baleeiro17 <lucasdominato2@gmail.com>
2017-07-04 13:50:34 -0300

    Reposicao.

---
+++ Sources/MODELO/reposicao.h
@@ -0,0 +1,19 @@
+/**
+ * Este módulo define a struct Reposicao e é responsável pelas funções de manipulações 
+ * da struct.
+ *
+ * @author Fernando Júnior Gomes da Silva <fernandojunior20110@gmail.com>
+ * @date 27/06/2017
+*/
+ 
+#include <time.h>
+
+#include "funcionario.h"
+#include "produto.h"
+  
+typedef struct {
+ 	Funcionario funcionario;
+ 	Produto produto;
+ 	struct tm horario;
+ 	int quantidade;
+}Reposicao;
